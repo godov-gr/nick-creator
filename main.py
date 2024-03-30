bc66ce0cbbdb330d4195c694cba85a986d4b320c
Godov <alexgodovk@gmail.com>
2024-03-31 01:23:06 +0500

    made scheme word + word + num

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import random
 import string
 
 def generate_readable_nickname(n):
-    words = ["Sky", "Fire", "Ice", "Wind", "Earth", "Shadow", "Light", "Water", "Storm", "Star"]
+    nouns = ["sky", "fire", "ice", "wind", "earth", "shadow", "light", "water", "storm", "star", "bolt", "wolf", "rabbit", "wild", "demon", "hunter", "soul", "nut", "light", "darkness", "deep"
+             , "rider", "dream", "men", "dog", "cat", "elf", "troll", "baby", "frog", "god", "mrak", "smoke", "drink", "next", "sniper", "dragon", "spirit", "beast", "doctor", "force", 
+             "mad", "missle", "ghoul", "dude", "bird", "bro", "eye", "face", "hand", "king", "lion", "life", "person", "piece", "place", "rock", "ship", "sound", "state"]
+    adjectives = ["sky", "fire", "ice", "wind", "earth", "shadow", "light", "water", "critical", "star", "agressive", "bad", "additional", "blue", "black", "red", "white", "hard", "light", "cool", "cold", "alive", 
+                  "angry", "crazy", "basic", "boring", "central", "clear", "common", "curious", "current", "dark", "deep", "dead", "desperate", "early", "easy", "every", "final", "free", "freedom",
+                   "general", "global", "great", "high", "hot", "huge", "impressive", "large", "late", "little", "local", "lucky", "main", "mantal", "military", "old", "hyper"]
+    cool_numbers = ["13", "69", "228", "420", "666", "777", "911", ]
     nicknames = []
     for _ in range(n):
-        word = random.choice(words)
-        number = str(random.randint(1, 99))
-        nickname = word + number
+        noun = random.choice(nouns)
+        cool_number = random.choice(cool_numbers)
+        adjective = random.choice(adjectives)
+        nickname = adjective + noun + cool_number
         nicknames.append(nickname)
     return nicknames
 
@@ -18,10 +25,8 @@ def save_nicknames_to_file(nicknames, filename):
 
 # Генерируем 100 никнеймов
 nicknames = generate_readable_nickname(100)
-# Сохраняем никнеймы в файл (пример для локального использования: 'nicknames.txt')
+# Сохраняем никнеймы в файл
 filename = 'nicknames.txt'
 save_nicknames_to_file(nicknames, filename)
 
 print(f"Nicknames saved to {filename}")
-
-#сделать так, чтобы числа использовались только 2/4/69 типа 2- ту, 4 - фор игра слов и добавить схему слово-цифра-слово или слово-слово-цифра
